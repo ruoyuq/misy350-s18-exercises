72e90f5615c09c9107707b058d9887d20c8c121d
ruoyuq <36270918+ruoyuq@users.noreply.github.com>
2018-04-05 10:40:03 -0400

    Add files via upload

---
+++ my-code.js
@@ -0,0 +1,10 @@
+blist = ["end of the semester","no research","Disen gets better"];
+console.log(blist.length);
+blist.push("get a beer");
+console.log(blist.length);
+console.log(blist);
+
+//for loop
+for(i=0; i<4; i++){
+   console.log(blist[i]);
+}
